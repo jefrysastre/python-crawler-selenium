bfce275cdfe82756bb535ebc64a382d2bdfbea15
Jefry Sastre <jefry.sastre@gmail.com>
2017-11-09 15:46:05 -0200

    - Adds parameter 'daemon_mode' to Crawler constructor in order to crawl in background.

--- crawler/crawler/crawler.py
+++ crawler/crawler/crawler.py
@@ -3,10 +3,14 @@ from selenium import webdriver
 import crawler.nodes as nodes
 
 class Crawler:
-    def __init__(self, config):
+    def __init__(self, config, daemon_mode = False):
         self.root = self.parse(config)
         self.get_state = lambda: self.root.get_state()
         self.set_state = lambda state: self.root.set_state(state)
+        if daemon_mode:
+            self.driver = webdriver.PhantomJS()
+        else:
+            self.driver = webdriver.Chrome()
 
     def parse(self, config):
         return nodes.node_mapping[config._type](
@@ -14,6 +18,5 @@ class Crawler:
             **config.__dict__)
 
     def run(self):
-        driver = webdriver.Chrome()
-        for data in self.root.execute(driver):
+        for data in self.root.execute(self.driver):
             yield data
